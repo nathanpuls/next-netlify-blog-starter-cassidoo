963039a49834b4997204a97196dab4d7a4e5904a
Nathan Puls <71989998+nathanpuls@users.noreply.github.com>
2021-01-03 16:40:20 -0600

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,28 +4,10 @@ const About = ({ title, description, ...props }) => {
   return (
     <>
       <Layout pageTitle={`${title} | About`} description={description}>
-        <h1 className="title">Welcome to this demo blog!</h1>
+        <h1 className="title">Hi there!</h1>
 
         <p className="description">
-          This is a simple blog built with Next, easily deployable on{' '}
-          <a href="https://url.netlify.com/r1j6ybSYU">Netlify</a>.
-        </p>
-
-        <p>
-          You can check out the{' '}
-          <a href="https://github.com/cassidoo/next-netlify-blog-starter">
-            repo here.
-          </a>{' '}
-          If you'd like to build it yourself,{' '}
-          <a href="https://url.netlify.com/ByVW0bCF8">
-            here is a tutorial on how to do so
-          </a>
-          !
-        </p>
-
-        <p>
-          This project includes a basic layout and header, base styles, dynamic
-          routing with getStaticPaths, and posts saved as Markdown.
+          Learn more about me at {' ')<a href="nathanpuls.com"</a>
         </p>
       </Layout>
     </>
